72e5eaae77b74b985541558b11d930cc98f26e86
DeyviConde <iamdeyvi@gmail.com>
2019-07-11 16:04:19 -0500

    Feature/game ui (#12)
    * Add GameUI component
    
    * Add GameUI components
    
    * Add Image Container
    
    * Add Statement component: stage text
    
    * Add testing components
    
    * Add titles: task + stations
    
    * Fix styles for Statement component
    
    * Add game form for gameUI & simple asset
    
    * Add task row based on stage game
    
    * Add RowList maps all tasks and bundle them
    
    * Add border radius for image style
    
    * Add react for JSX statements
    
    * Add React for JSX statements
    
    * Add React for JSX & wrap tasks into a form
    
    * Fix radio decision & add react for JSX
    
    * Add GameUI component
    
    * Call GameUI into App
    
    * Add submit button
    
    * Add Send button

---
+++ simcase-game/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GameUI from "./views/GameUI";
+
+function App() {
+  return (
+    <div>
+      <GameUI />
+    </div>
+  );
+}
+
+export default App;
---
+++ simcase-game/src/components/TitlesForm.js
@@ -0,0 +1,39 @@
+/** @jsx jsx */
+import React from "react";
+import { jsx } from "@emotion/core";
+
+const container = {
+  background: "rgb(213,245,245)",
+  borderRadius: "4px",
+  display: "table",
+  tableLayout: "fixed",
+  width: "90%",
+  height: "4vh",
+  boxSizing: "border-box",
+  marginLeft: "auto",
+  marginRight: "auto",
+  marginTop: "16px", //Needs a little fix
+  marginBottom: "16px" // Must be automatic
+};
+
+const item = {
+  display: "table-cell",
+  verticalAlign: "middle",
+  textAlign: "center",
+  fontFamily: "Arial",
+  color: "blue",
+  fontSize: "1.0em"
+};
+
+function TitlesForm() {
+  return (
+    <div css={container}>
+      <div css={item}>Tasks</div>
+      <div css={item}>Station 1</div>
+      <div css={item}>Station 2</div>
+      <div css={item}>Station 3</div>
+    </div>
+  );
+}
+
+export default TitlesForm;
