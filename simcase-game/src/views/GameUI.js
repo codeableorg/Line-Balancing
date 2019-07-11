72577bc7d92d77b92366b2a0654a07fcc60e6912
byteconde <iamdeyvi@gmail.com>
2019-07-11 14:09:21 -0500

    Add GameUI component

---
+++ simcase-game/src/views/GameUI.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Statement from "../components/Statement";
+import ImageContainer from "../components/ImageContainer";
+import TitlesForm from "../components/TitlesForm";
+import TaskList from "../components/TaskRowList";
+
+function App() {
+  return (
+    <div>
+      <Statement />
+      <ImageContainer />
+      <TitlesForm />
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
