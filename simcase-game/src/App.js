5755691087b9bd1184595165a686f4eddb590b7d
Cesar Vangelis Cachay Arbildo <cesarcachay1996@gmail.com>
2019-07-12 11:35:46 -0500

    Feature/user navigation (#16)
    * Add portal & refactor modal
    
    * Render component inside the modal

--- simcase-game/src/App.js
+++ simcase-game/src/App.js
@@ -1,22 +1,49 @@
 /** @jsx jsx */
 import React from "react";
-import { jsx, Global } from "@emotion/core";
-import Welcome from "./components/Welcome";
-import Role from "./components/Role";
-import Walkthrough from "./components/Walkthrough";
-import Header from "./components/Header";
+import { jsx } from "@emotion/core";
+import { Router } from "@reach/router";
+import Welcome from "./views/Welcome";
+import Role from "./views/Role";
+import Walkthrough from "./views/Walkthrough";
+import GameUI from "./views/GameUI";
+import About from "./views/About";
+import Leaderboard from "./views/Leaderboard";
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+  function openModal() {
+    setIsModalOpen(true);
+  }
+
+  function closeModal() {
+    setIsModalOpen(false);
+  }
+
   return (
-    <>
-      <Global
-        styles={{ body: { margin: 0, padding: 0, fontFamily: "sans-serif" } }}
+    <Router>
+      <Welcome
+        path="/"
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+        openModal={openModal}
+        closeModal={closeModal}
       />
-      <Header />
-      <Welcome />
-      <Role />
-      <Walkthrough />
-    </>
+      <GameUI
+        path="game"
+        isModalOpen={isModalOpen}
+        openModal={openModal}
+        closeModal={closeModal}
+      >
+        <Role path="/role" />
+        <Walkthrough path="/walkthrough" />
+        <About path="/about" />
+      </GameUI>
+      <Role path="/role" />
+      <Walkthrough path="/walkthrough" />
+      <About path="/about" />
+      <Leaderboard path="/leaderboard" />
+    </Router>
   );
 }
 
--- simcase-game/src/views/Role.js
+++ simcase-game/src/views/Role.js
@@ -1,16 +1,30 @@
 /** @jsx jsx */
-
+import React from "react";
 import { jsx } from "@emotion/core";
+import { Link } from "@reach/router";
 
 function Role() {
   const contentStyle = {
     padding: 10
   };
+
+  const buttonBack = {
+    marginTop: 100,
+    padding: 20,
+    borderRadius: 5,
+    color: "white",
+    background: "blue",
+    textDecoration: "none"
+  };
+
   return (
     <main css={contentStyle}>
       <h2>Role</h2>
       <img src="assets/img/rolePic.gif" alt="Role" css={{ width: 300 }} />
       <p id="role">Role message for students</p>
+      <Link to="/" css={buttonBack}>
+        Go Back
+      </Link>
     </main>
   );
 }
