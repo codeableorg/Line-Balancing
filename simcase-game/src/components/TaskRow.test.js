33a6b39b7f77867a70cd617a73886e7ce28add2a
byteconde <iamdeyvi@gmail.com>
2019-07-12 11:57:43 -0500

    Add tests with emotion jest

---
+++ simcase-game/src/components/TaskRow.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import TaskRow from "./TaskRow";
+test("TaskRow based on game stage", () => {
+  const { asFragment } = render(<TaskRow />);
+  expect(asFragment()).toMatchSnapshot();
+});
