b68c019b184002b8d6bc475615da728c9f3de7f6
Carlos A <cayala.w@gmail.com>
2019-07-15 19:25:11 -0500

    Ad buttons to navigate modals

--- simcase-game/src/views/Role.js
+++ simcase-game/src/views/Role.js
@@ -3,19 +3,45 @@ import React from "react";
 import { jsx } from "@emotion/core";
 import { Dialog } from "@reach/dialog";
 import Component from "@reach/component-component";
-import "@reach/dialog/styles.css";
+// import "@reach/dialog/styles.css";
+
+import { createPortal } from "react-dom";
+
+import Walkthrough from "./Walkthrough";
 
 const contentStyle = {
   padding: 10
 };
 
+const resetDialog = { width: "100vw", height: "100vh", padding: 0, margin: 0 };
+
+const closeButton = {
+  position: "fixed",
+  top: 10,
+  right: 10,
+  border: "none",
+  padding: 10,
+  margin: 0,
+  borderRadius: "50%",
+  backgroundColor: "#004785",
+  color: "#FFFFFF",
+  fontWeight: "bold"
+};
+
 function Role(props) {
-  const { onClose, openComponent, isComponentOpen } = props;
+  const $portal = document.getElementById("portal");
+  const { onClose, openComponent, closeComponent, isComponentOpen } = props;
+
+  const [modal, setModal] = React.useState(null);
 
   return (
     <Component isOpen={isComponentOpen}>
       <div>
-        <Dialog isOpen={openComponent} onDismiss={openComponent}>
+        <Dialog
+          isOpen={openComponent}
+          onDismiss={openComponent}
+          css={resetDialog}
+        >
           <main css={contentStyle}>
             <h2>Role</h2>
             <img
@@ -24,7 +50,26 @@ function Role(props) {
               css={{ width: 300 }}
             />
             <p id="role">Role message for students</p>
-            <button onClick={onClose}>X</button>
+            <button onClick={onClose} css={closeButton}>
+              X
+            </button>
+
+            <button
+              onClick={() => {
+                setModal("walkthrough");
+              }}
+            >
+              Return to Walkthrough
+            </button>
+            {modal === "walkthrough" &&
+              createPortal(
+                <Walkthrough
+                  component={isComponentOpen}
+                  onClose={closeComponent}
+                  openModal={openComponent}
+                />,
+                $portal
+              )}
           </main>
         </Dialog>
       </div>
--- simcase-game/src/views/Walkthrough.js
+++ simcase-game/src/views/Walkthrough.js
@@ -4,17 +4,46 @@ import { jsx } from "@emotion/core";
 import { Dialog } from "@reach/dialog";
 import Component from "@reach/component-component";
 import "@reach/dialog/styles.css";
+import { createPortal } from "react-dom";
+import Role from "./Role";
 
 function Walkthrough(props) {
-  const { onClose, openComponent, isComponentOpen } = props;
+  const $portal = document.getElementById("portal");
+  const { onClose, openComponent, closeComponent, isComponentOpen } = props;
+
+  const [modal, setModal] = React.useState(null);
 
   const contentStyle = {
     padding: 10
   };
+  const resetDialog = {
+    width: "100vw",
+    height: "100vh",
+    padding: 0,
+    margin: 0
+  };
+
+  const closeButton = {
+    position: "fixed",
+    top: 10,
+    right: 10,
+    border: "none",
+    padding: 10,
+    margin: 0,
+    borderRadius: "50%",
+    backgroundColor: "#004785",
+    color: "#FFFFFF",
+    fontWeight: "bold"
+  };
+
   return (
     <Component isOpen={isComponentOpen}>
       <div>
-        <Dialog isOpen={openComponent} onDismiss={openComponent}>
+        <Dialog
+          isOpen={openComponent}
+          onDismiss={openComponent}
+          css={resetDialog}
+        >
           <main css={contentStyle}>
             <h2>Walkthrough</h2>
             <img
@@ -28,7 +57,25 @@ function Walkthrough(props) {
               <li>Some more text</li>
               <li>Finally the end og the list</li>
             </ul>
-            <button onClick={onClose}>X</button>
+            <button onClick={onClose} css={closeButton}>
+              X
+            </button>
+            <button
+              onClick={() => {
+                setModal("role");
+              }}
+            >
+              Go to Role >>
+            </button>
+            {modal === "role" &&
+              createPortal(
+                <Role
+                  component={isComponentOpen}
+                  onClose={closeComponent}
+                  openModal={openComponent}
+                />,
+                $portal
+              )}
           </main>
         </Dialog>
       </div>
