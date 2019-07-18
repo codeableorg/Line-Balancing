12389aacad76db2447eddd0123fa4730ab00d717
paulotijero <paulotijero@gmail.com>
2019-07-18 15:42:16 -0500

    Fix rebase

--- simcase-game/src/views/Walkthrough.js
+++ simcase-game/src/views/Walkthrough.js
@@ -5,30 +5,22 @@ import { Dialog } from "@reach/dialog";
 import Component from "@reach/component-component";
 
 function Walkthrough(props) {
-  const { onClose, openComponent, isComponentOpen } = props;
-
   return (
-    <Component isOpen={isComponentOpen}>
-      <div>
-        <Dialog isOpen={openComponent} onDismiss={openComponent}>
-          <main>
-            <h2>Walkthrough</h2>
-            <img
-              src="/assets/img/walkthrough.gif"
-              alt="Walkthrough to use the app"
-            />
-            <ul id="walkthrough">
-              <li>Read the problem statement</li>
-              <li>Check the table</li>
-              <li>Some text</li>
-              <li>Some more text</li>
-              <li>Finally the end og the list</li>
-            </ul>
-            <button onClick={onClose}>X</button>
-          </main>
-        </Dialog>
-      </div>
-    </Component>
+    <main>
+      <h2>Walkthrough</h2>
+      <img
+        src="/assets/img/walkthrough.gif"
+        alt="Walkthrough to use the app"
+        css={{ display: "block", margin: "0 auto" }}
+      />
+      <ul id="walkthrough">
+        <li>Read the problem statement</li>
+        <li>Check the table</li>
+        <li>Some text</li>
+        <li>Some more text</li>
+        <li>Finally the end og the list</li>
+      </ul>
+    </main>
   );
 }
 
