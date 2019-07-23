6bc6f9f576ab17b93fe1f6dcb91b6e062fbd9100
Cesar Cachay <cesarcachay1996@gmail.com>
2019-07-23 18:01:32 -0500

    Render addtion of time per station

--- simcase-game/src/components/TaskList.js
+++ simcase-game/src/components/TaskList.js
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 import React from "react";
 import { jsx } from "@emotion/core";
+import React from "react";
 
 import tasksJson from "../data/tasks.json";
 import { Table } from "../components/ui";
