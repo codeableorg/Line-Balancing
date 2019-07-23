495fae6a8ae37b137f54800d9617e8d839dedb8a
Cesar Cachay <cesarcachay1996@gmail.com>
2019-07-23 18:01:32 -0500

    Render addtion of time per station

--- simcase-game/src/components/TaskList.js
+++ simcase-game/src/components/TaskList.js
@@ -1,44 +1,92 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
+import React from "react";
 
 import tasksJson from "../data/tasks.json";
 import { Table } from "../components/ui";
 
+/**
+ * {
+ *   task0: 1,
+ *   task1: 2,
+ *   task2: 3
+ * }
+ * [["task0", 1], ["task1", 2], ["task2", 3]]
+ */
+
 function TaskList({ id }) {
-  const tasks = Object.values(tasksJson.scenarios[id].tasks);
+  const tasks = Object.entries(tasksJson.scenarios[id].tasks);
+  const [tasksPerStation, setTasksPerStation] = React.useState({});
+
+  function addToStation(number) {
+    return event => {
+      const id = event.target.value;
+      setTasksPerStation(current => ({
+        ...current,
+        [id]: number
+      }));
+    };
+  }
+
+  console.log(tasksPerStation);
+
+  const timesPerStation = Object.entries(tasksPerStation)
+    .map(([taskId, stationNumber]) => {
+      return [tasksJson.scenarios[id].tasks[taskId].time, stationNumber];
+    }) // [[10, 1], [5, 2], [3, 2]]
+    .reduce((total, [time, stationNumber]) => {
+      return {
+        ...total,
+        [stationNumber]: total[stationNumber]
+          ? total[stationNumber] + time
+          : time
+      };
+    }, {}); // { 1: 10, 2: 8 }
 
   return (
     <form>
-      {tasks.map((task, index) => {
+      {tasks.map(([taskId, task]) => {
         return (
-          <Table key={`key_${index}`}>
+          <Table key={`key_${taskId}`}>
             <div>
               {task.name} ({task.time})
             </div>
             <div>
               <input
                 type="radio"
-                name={`${id}_task_${index + 1}`}
+                name={`${id}_task_${taskId}`}
                 id={`${id}_station_1`}
+                onChange={addToStation(1)}
+                value={taskId}
               />
             </div>
             <div>
               <input
                 type="radio"
-                name={`${id}_task_${index + 1}`}
+                name={`${id}_task_${taskId}`}
                 id={`${id}_station_2`}
+                onChange={addToStation(2)}
+                value={taskId}
               />
             </div>
             <div>
               <input
                 type="radio"
-                name={`${id}_task_${index + 1}`}
+                name={`${id}_task_${taskId}`}
                 id={`${id}_station_3`}
+                onChange={addToStation(3)}
+                value={taskId}
               />
             </div>
           </Table>
         );
       })}
+      Station 1:
+      {timesPerStation[1]}
+      Statin 2:
+      {timesPerStation[2]}
+      Station 3:
+      {timesPerStation[3]}
     </form>
   );
 }
