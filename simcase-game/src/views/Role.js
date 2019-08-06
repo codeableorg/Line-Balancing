980c5dd45c8c3c2a8315638fa7974fd718999369
Cesar Cachay <cesarcachay1996@gmail.com>
2019-08-06 16:09:07 -0500

    Add real data from Renzo

--- simcase-game/src/views/Role.js
+++ simcase-game/src/views/Role.js
@@ -7,12 +7,53 @@ const img = {
   margin: "0 auto"
 };
 
+const info = {
+  textAlign: "center",
+  color: "#747474",
+  lineHeight: "24px",
+  p: {
+    width: "232px",
+    "&:first-of-type": {
+      fontWeight: "bold",
+      fontSize: "24px"
+    }
+  }
+};
+
+const container = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "space-evenly",
+  height: "100%"
+};
+
+const titleRole = {
+  width: "337.5px",
+  display: "flex",
+  justifyContent: "center",
+  backgroundColor: "#DAC4FF"
+};
+
 function Role() {
   return (
-    <main>
-      <h2>Role</h2>
-      <img css={img} src="/assets/img/rolePic.gif" alt="Role" />
-      <p id="role">Role message for students</p>
+    <main css={container}>
+      <div css={titleRole}>
+        <h2 css={{ color: "#44056E" }}>ROLE</h2>
+      </div>
+      <div css={info}>
+        <p>This is have 4 different scenarios</p>
+        <p>
+          You are a restaurant advisor helping food truck owners optimize their
+          operations. You always begin by line balancing since it requires
+          relatively little investment but can increase throughput and revenue.
+        </p>
+        <p>
+          To align incentives, you use a performance-based fee. That means your
+          fee grows as the trucks produce more revenue. Compete with your peers
+          to see who can generate the most shared upside!
+        </p>
+      </div>
     </main>
   );
 }
--- simcase-game/src/views/Walkthrough.js
+++ simcase-game/src/views/Walkthrough.js
@@ -2,21 +2,54 @@
 import { jsx } from "@emotion/core";
 
 function Walkthrough(props) {
+  const info = {
+    textAlign: "center",
+    color: "#747474",
+    lineHeight: "24px",
+    p: {
+      width: "232px",
+      "&:first-of-type": {
+        fontWeight: "bold",
+        fontSize: "24px"
+      }
+    }
+  };
+
+  const container = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    height: "100"
+  };
+
+  const titleRole = {
+    width: "337.5px",
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "#DAC4FF"
+  };
+
   return (
-    <main>
-      <h2>Walkthrough</h2>
-      <img
-        src="/assets/img/walkthrough.gif"
-        alt="Walkthrough to use the app"
-        css={{ display: "block", margin: "0 auto" }}
-      />
-      <ul id="walkthrough">
-        <li>Read the problem statement</li>
-        <li>Check the table</li>
-        <li>Some text</li>
-        <li>Some more text</li>
-        <li>Finally the end og the list</li>
-      </ul>
+    <main css={container}>
+      <div css={titleRole}>
+        <h2 css={{ color: "#44056E" }}>Walkthrough</h2>
+      </div>
+      <div css={info}>
+        <p css={{ color: "#44056E" }}>Step 1</p>
+        <p>Review the activity time for each stage in the process.</p>
+      </div>
+      <div css={info}>
+        <p css={{ color: "#44056E" }}>Step 2</p>
+        <p>
+          Shift adjacent tasks from one stage to another. Note that activities
+          must be completed sequentially.
+        </p>
+      </div>
+      <div css={info}>
+        <p css={{ color: "#44056E" }}>Step 3</p>
+        <p>Determine the optimal arrangement of tasks to maximize throughput</p>
+      </div>
     </main>
   );
 }
